c803b71596c6f8e3e06efce8c684b9b56d7b5073
hellosiyam <hellosiyambd@gmail.com>
2025-02-04 15:04:55 +0600

    mi-7-mo-39-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
 function App() {
+  
+  const alertBtn = () => {
+    alert('Button Clicked')
+  }
 
+  const alertBtnWithPera = (num) => {
+    alert(num + 5)
+  }
   return (
     <>
       <h1>React</h1>
+      <button onClick={alertBtn}>On click </button>
+      <button onClick={() => {alert('On click Btn 2')}}>On click Btn 2</button>
+      <button onClick={() => {alertBtnWithPera(10)}}>On click Btn 3</button>
     </>
   )
 }
